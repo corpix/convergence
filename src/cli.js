68da023cbebca89c4bf73fb365b0f0f23d51f598
Dmitry Moskowski <me@corpix.ru>
2015-12-12 17:47:29 +0300

    Moved to docopt

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,17 @@
 var path = require('path');
 var assert = require('assert');
+var docopt = require('docopt').docopt;
+var package = require('../package');
 var app = require('./main');
+
+var cli = [
+    'Usage:',
+    '  convergence (-l <levels>|--levels <levels>...) <decls>...',
+    '',
+    '    -l <levels>, --levels <levels> is a path to BEM level',
+    '    <decls>                        is a path to BEM decl file',
+    ''
+].join('\n');
 
 function sanitizeLevel(s) {
     return (s || '').replace(/\/$/, '');
@@ -11,39 +22,18 @@ function logJSON(d) {
 }
 
 function main(argv) {
-    var args = require('minimist')(argv.slice(2));
-    var res = {_: args._};
-    if (args.h || args.help) {
-        usage();
-    }
-    if (args._.length === 0) {
-        usage(1);
-    } else {
-        res.decls = args._;
-    }
+    var args = docopt(cli, {
+        argv: argv.slice(2),
+        version: package.version
+    });
 
-    res.levels = args.l || args.levels;
-    if (typeof res.levels === 'string') {
-        res.levels = [res.levels];
-    }
-    assert(Array.isArray(res.levels), 'Levels should be a list of strings');
-    res.levels = res.levels.map(sanitizeLevel);
+    var params = {};
+    params.levels = args['<levels>'].map(sanitizeLevel);
+    params.decls = args['<decls>'];
 
-    return app.main(res).then(logJSON);
-}
-
-function usage(exitCode) {
-    var space = '    ';
-    process.stderr.write([
-        '',
-        space + 'Usage: ' + path.basename(process.argv[1]) + ' [-l level/path ...] path/to/bemdecl.js',
-        '',
-        '',
-    ].join('\n'));
-    process.exit(exitCode || 0);
+    return app.main(params).then(logJSON);
 }
 
 module.exports = {
-    main: main,
-    usage: usage
+    main: main
 };
